9e32d25354baec5a84b9d184ffbb9187d16b388d
Bulgun <bulgunya@mail.ru>
2024-03-29 21:19:16 +0300

    013 внедрение компонентов в HomePage

--- client/src/layouts/HomePage/HomePage.tsx
+++ client/src/layouts/HomePage/HomePage.tsx
@@ -1,9 +1,15 @@
+import Carousel from "./components/Carousel";
 import ExploreBooks from "./components/ExploreBooks";
+import CallAction from "./components/CallAction";
+import LibraryServices from "./components/LibraryServices";
 
 function HomePage() {
   return (
     <div>
       <ExploreBooks/>
+      <Carousel />
+      <CallAction />
+      <LibraryServices />
     </div>
   );
 }
